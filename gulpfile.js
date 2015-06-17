ef8cacc325aaec4d6509c904a97d0a5bb5e2be2e
Valentin Voilean <valentin.voilean@netcentric.biz>
2015-06-17 15:20:47 +0200

    created first coffeescript file

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 // https://github.com/unlight/gulp-cssimport
 var gulp = require('gulp'),
     cssimport = require("gulp-cssimport"),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
 
-var globalConfig = {
-  src: '_DEV_/SCSS' // your dev stylesheet directory. No trailing slash
-};
+    sass_folder = { src: '_DEV_/SCSS'},
+    coffee_folder = { src: '_DEV_/JS' };
 
 // Process CSS
 gulp.task('styles', function(){
-  return gulp.src(globalConfig.src + '/**/*.*')
+  return gulp.src(sass_folder.src + '/**/*.*')
     .pipe(cssimport())
-    .pipe(gulp.dest('assets/'));
+    .pipe(gulp.dest('./assets/'));
+});
+
+gulp.task('coffee', function() {
+  gulp.src(coffee_folder.src + '/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./assets/'))
 });
 
 // Watch files
 gulp.task('watch', function () {
-  gulp.watch(globalConfig.src + '/**/*.*', ['styles']);
+  gulp.watch(sass_folder.src + '/**/*.*', ['styles']);
+  gulp.watch(coffee_folder.src + '/*.coffee', ['coffee']);
 });
 
 // Default task
